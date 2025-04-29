2ee958fa116615babd4f70022e088c58d1fff7a0
Aachalraut <124812816+Aachalraut@users.noreply.github.com>
2025-04-29 07:40:31 +0530

    Update polariser.py

--- polariser.py
+++ polariser.py
@@ -9,6 +9,16 @@ from textblob import TextBlob
 nltk.download('vader_lexicon')
 nlp = spacy.load('en_core_web_sm')
 nlp.add_pipe('spacytextblob')
+import spacy
+from spacy.cli import download
+
+# Download the model if not available
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
+
 
 def get_polarity(text):
     sia = SentimentIntensityAnalyzer()
